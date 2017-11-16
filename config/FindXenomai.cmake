4b9489718af2d00e01e253f0cd130d4592ae4a39
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-11-16 14:42:26 +0100

    update doc

--- config/FindXenomai.cmake
+++ config/FindXenomai.cmake
@@ -1,30 +1,52 @@
 ################################################################################
-#
-# CMake script for finding the XENOMAI 2 native or XENOMAI 3 alchemy skin.
+# Copyright 2017 Antoine HOARAU <hoarau.robotics@gmail.com>
+# 
+# CMake script for finding the XENOMAI 2 native or XENOMAI 3 alchemy/posix/rtdm skin.
 # If the optional XENOMAI_ROOT_DIR environment variable exists, header files and
 # libraries will be searched in the XENOMAI_ROOT_DIR/include and XENOMAI_ROOT_DIR/lib
 # directories, respectively. Otherwise the default CMake search process will be
 # used.
 #
-# This script creates the following variables:
+# This script creates the following variables for each skin.
+#
+#  Native/Alchemy skin :
+#
 #  XENOMAI_FOUND: Boolean that indicates if the package was found
 #  XENOMAI_INCLUDE_DIRS: Paths to the necessary header files
 #  XENOMAI_LIBRARIES: Package libraries
+#  XENOMAI_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_LD_FLAGS: Package linker flags for additional parsing
+#  XENOMAI_COMPILE_DEFINITIONS: Package compile flags
 #
+#  And it also set the XENOMAI_POSIX_* and XENOMAI_RTDM_* variables.
+#
+#  Posix Interface : 
+#
+#  XENOMAI_POSIX_FOUND: Boolean that indicates if the package was found
+#  XENOMAI_POSIX_INCLUDE_DIRS: Paths to the necessary header files
+#  XENOMAI_POSIX_LIBRARIES: Package libraries
+#  XENOMAI_POSIX_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_POSIX_LD_FLAGS: Package linker flags for additional parsing
+#  XENOMAI_POSIX_COMPILE_DEFINITIONS: Package compile flags
+#
+#  RTDM Interface :
+#
+#  XENOMAI_RTDM_FOUND: Boolean that indicates if the package was found
+#  XENOMAI_RTDM_INCLUDE_DIRS: Paths to the necessary header files
+#  XENOMAI_RTDM_LIBRARIES: Package libraries
+#  XENOMAI_RTDM_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_RTDM_LD_FLAGS: Package linker flags for additional parsing
+#  XENOMAI_RTDM_COMPILE_DEFINITIONS: Package compile flags
+#
+#  NOTE: You still need FindRTnet.cmake for rtnet support on xenomai 2.x
 ################################################################################
+
+include(FindPackageHandleStandardArgs)
 
 # Get hint from environment variable (if any)
 if(NOT $ENV{XENOMAI_ROOT_DIR} STREQUAL "")
     set(XENOMAI_ROOT_DIR $ENV{XENOMAI_ROOT_DIR} CACHE PATH "Xenomai base directory location (optional, used for nonstandard installation paths)" FORCE)
     mark_as_advanced(XENOMAI_ROOT_DIR)
-endif()
-
-if (Xenomai_FIND_QUIETLY)
-    set(XENOMAI_FIND_QUIETLY "QUIET")
-endif()
-
-if (Xenomai_FIND_REQUIRED)
-    set(XENOMAI_FIND_REQUIRED "REQUIRED")
 endif()
 
 # Find headers and libraries
@@ -36,9 +58,10 @@ else()
     find_program(XENOMAI_XENO_CONFIG NAMES xeno-config )
 endif()
 
-function(find_xeno_skin_variables prefix skin_name)
-    message(STATUS "Xenomai ${${prefix}_VERSION} detected, searching for ${skin_name} skin.")
+mark_as_advanced(XENOMAI_XENO_CONFIG)
 
+function(find_xeno_skin_variables prefix skin_name)
+    
     execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=${skin_name} --ldflags ${XENO_CONFIG_LDFLAGS_EXTRA_ARGS}
                     OUTPUT_VARIABLE ${prefix}_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE
                     ERROR_VARIABLE ${prefix}_LDFLAGS_ERROR)
@@ -65,33 +88,28 @@ function(find_xeno_skin_variables prefix skin_name)
     string(REGEX MATCHALL "-D([^ ]+)" ${prefix}_COMPILE_DEFINITIONS ${${prefix}_CFLAGS})
     string(REPLACE "-I" ";" ${prefix}_INCLUDE_DIR ${${prefix}_INCLUDE_DIR})
 
-    message(STATUS "
---------------------------------------------------------------------------------
-    Xenomai ${XENOMAI_VERSION} ${skin_name} skin
-        libs    : ${${prefix}_LIBRARY}
-        include : ${${prefix}_INCLUDE_DIR}
-        ldflags : ${${prefix}_LDFLAGS}
-        cflags  : ${${prefix}_CFLAGS}")
-
-        set(${prefix}_INCLUDE_DIRS ${${prefix}_INCLUDE_DIR} CACHE INTERNAL "")
-        set(${prefix}_LIBRARIES ${${prefix}_LIBRARY} CACHE INTERNAL "")
-        set(${prefix}_COMPILE_DEFINITIONS ${${prefix}_COMPILE_DEFINITIONS} CACHE INTERNAL "")
-        set(${prefix}_LDFLAGS ${${prefix}_LDFLAGS} CACHE INTERNAL "")
-        set(${prefix}_CFLAGS ${${prefix}_CFLAGS} CACHE INTERNAL "")
-        set(${prefix}_FOUND ${${prefix}_FOUND} CACHE INTERNAL "")
-
-        mark_as_advanced(${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS ${prefix}_COMPILE_DEFINITIONS ${prefix}_CFLAGS ${prefix}_LDFLAGS)
-
-    message(STATUS "
---------------------------------------------------------------------------------
+    message(STATUS "Xenomai ${XENOMAI_VERSION} ${skin_name} skin
+    libs    : ${${prefix}_LIBRARY}
+    include : ${${prefix}_INCLUDE_DIR}
+    ldflags : ${${prefix}_LDFLAGS}
+    cflags  : ${${prefix}_CFLAGS}
     ")
+
+    set(${prefix}_INCLUDE_DIRS ${${prefix}_INCLUDE_DIR} CACHE INTERNAL "")
+    set(${prefix}_LIBRARIES ${${prefix}_LIBRARY} CACHE INTERNAL "")
+    set(${prefix}_COMPILE_DEFINITIONS ${${prefix}_COMPILE_DEFINITIONS} CACHE INTERNAL "")
+    set(${prefix}_LDFLAGS ${${prefix}_LDFLAGS} CACHE INTERNAL "")
+    set(${prefix}_CFLAGS ${${prefix}_CFLAGS} CACHE INTERNAL "")
+    set(${prefix}_FOUND ${${prefix}_FOUND} CACHE INTERNAL "")
+
+    mark_as_advanced(${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS ${prefix}_COMPILE_DEFINITIONS ${prefix}_CFLAGS ${prefix}_LDFLAGS)
 endfunction()
 
 function(handle_standard_args prefix)
     find_package_handle_standard_args(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS ${prefix}_COMPILE_DEFINITIONS ${prefix}_CFLAGS ${prefix}_LDFLAGS)
 endfunction()
 
-if(XENOMAI_XENO_CONFIG )
+if(XENOMAI_XENO_CONFIG)
     # Detect Xenomai version
     execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --version OUTPUT_VARIABLE XENOMAI_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
     string(REPLACE "." ";" XENOMAI_VERSION_LIST ${XENOMAI_VERSION} )
@@ -126,3 +144,5 @@ endif()
 handle_standard_args(XENOMAI)
 handle_standard_args(XENOMAI_POSIX)
 handle_standard_args(XENOMAI_RTDM)
+
+find_package_handle_standard_args(Xenomai DEFAULT_MSG XENOMAI_XENO_CONFIG)
